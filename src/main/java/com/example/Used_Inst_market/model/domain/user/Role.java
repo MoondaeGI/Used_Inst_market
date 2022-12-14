d10b2870ea410fa105f14748d9c1a372fa9a19e5
MoondaeGI <ansgur119977@gmail.com>
2022-12-14 15:57:25 +0900

    delete session dependency while find solution

--- src/main/java/com/example/Used_Inst_market/model/domain/user/Role.java
+++ src/main/java/com/example/Used_Inst_market/model/domain/user/Role.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    GUEST("ROLE_GUEST", "게스트 계정"),
+    GUEST("ROLE_ADMIN", "관리자 계정"),
     USER("ROLE_USER", "유저 계정");
 
     private final String key;
