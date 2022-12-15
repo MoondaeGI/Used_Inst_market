8e643fcdee60daa813bd5da2e4b51e63e6521fdc
MoondaeGI <ansgur119977@gmail.com>
2022-12-16 00:46:58 +0900

    Add swagger annotation to PictureDTO

--- src/main/java/com/example/Used_Inst_market/web/dto/board/picture/PictureUpdateRequestDTO.java
+++ src/main/java/com/example/Used_Inst_market/web/dto/board/picture/PictureUpdateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.Used_Inst_market.web.dto.board.picture;
 
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,11 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class PictureUpdateRequestDTO {
+    @ApiParam(name = "게시글 번호", required = true,
+            value = "postNo", example = "1")
     private Long postNo;
+    @ApiParam(name = "이미지 파일 리스트", required = true,
+            value = "multipartFiles", example = "[1, 1, 1, 1]")
     private List<MultipartFile> multipartFiles;
 
     @Builder
