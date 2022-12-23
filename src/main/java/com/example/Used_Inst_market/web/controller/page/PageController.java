2b14cdb96b18d04054fe22db868fb5c0b9fbf6ab
MoondaeGI <ansgur119977@gmail.com>
2022-12-23 14:00:38 +0900

    Add PageController for display pages

---
+++ src/main/java/com/example/Used_Inst_market/web/controller/page/PageController.java
@@ -0,0 +1,42 @@
+package com.example.Used_Inst_market.web.controller.page;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = {"화면 API"})
+@RestController
+public class PageController {
+    @ApiOperation(value = "메인 페이지 조회 API")
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @ApiOperation(value = "게시글 페이지 조회 API")
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/post")
+    public String post() {
+        return "post";
+    }
+
+    @ApiOperation(value = "게시글 등록 페이지 조회 API")
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/post/save")
+    public String postSave() { return "post-save"; }
+
+    @ApiOperation(value = "게시글 수정 페이지 조회 API")
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/post/update")
+    public String postUpdate() { return "post-update"; }
+
+    @ApiOperation(value = "검색 결과 페이지 조회 API")
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/search")
+    public String search() {
+        return "search";
+    }
+}
